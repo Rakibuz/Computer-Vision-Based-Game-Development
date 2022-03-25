cee4b4709549e0dbe0ca7fef361f603fa1f9aaaa
Md. Rakibuz Sultan Pranto <pranto055@gmail.com>
2022-03-25 23:12:42 +0600

    Update Drowsiness_Detection.py

--- Drowsiness_Detection/Drowsiness_Detection.py
+++ Drowsiness_Detection/Drowsiness_Detection.py
@@ -13,10 +13,21 @@ drawSpec = mpDraw.DrawingSpec(thickness=1, circle_radius=2)
 
 cap = cv2.VideoCapture(0)
 pTime = 0
-plotY = LivePlot(640,360,[20,50])
+plotY = LivePlot(640,360,[20,50],invert=True)
 
+#--------------------------Right Eye-----------------------
+#idList=[22,23,24,26,110,157,158,159,160,161,130,243]
 
-idList=[22,23,24,26,110,157,158,159,160,161,130,243]
+#-----------------------------LeftEye-----------------------
+#idList=[466,388,387,386,385,384,398,263,249,390,373,374,380,381,382,362]
+
+#---------------------------------Combine------------------------
+idList=[22,23,24,26,110,157,158,159,160,161,130,243,466,388,387,386,385,384,398,263,249,390,373,374,380,381,382,362]
+
+ratiolist=[]
+blinkCounter=0
+counter=0
+color=(255,0,255)
 
 def findDistance(p1,p2):
      distance = math.sqrt( ((p1[0]-p2[0])**2)+((p1[1]-p2[1])**2) )
@@ -47,21 +58,39 @@ while True:
             if faces:
                 face=faces[0]
                 for id in idList:
-                    cv2.circle(img,tuple(face[id]),2,(255,0,255),cv2.FILLED)
+                    cv2.circle(img,tuple(face[id]),2,color,cv2.FILLED)
 
                 leftUp=face[159]
                 leftDown=face[23]
                 leftLeft=face[130]
                 leftRight=face[243]
                 #print(leftUp)
-                cv2.line(img,tuple(leftUp),tuple(leftDown),(0,200,0),3)
-                cv2.line(img,tuple(leftLeft),tuple(leftRight),(0,200,0),3)
+                #cv2.line(img,tuple(leftUp),tuple(leftDown),(0,200,0),3)
+                #cv2.line(img,tuple(leftLeft),tuple(leftRight),(0,200,0),3)
                 length_Ver=findDistance(leftUp,leftDown)
                 length_Hor=findDistance(leftLeft,leftRight)
 
-                ratio=(length_Ver/length_Hor)*100
+                ratio=int((length_Ver/length_Hor)*100)
+                ratiolist.append(ratio)
                 
-                imgPlot=plotY.update(ratio)
+                if len(ratiolist)>3:
+                    ratiolist.pop(0)
+                
+                ratioAvg=sum(ratiolist)/len(ratiolist)
+                
+                if ratioAvg<35 and counter==0:
+                    blinkCounter +=1
+                    color=(0,200,0)
+                    counter=1
+                if counter !=0:
+                    counter +=1
+                    if counter>10:
+                        counter=0
+                        color=(255,0,255)
+                
+                cvzone.putTextRect(img,f'Blink Count:{blinkCounter}',(50,100),colorR=color)
+                   
+                imgPlot=plotY.update(ratioAvg,color)
                 img=cv2.resize(img,(640,360))
                 imgStack=cvzone.stackImages([img,imgPlot],2,1)
             else:
