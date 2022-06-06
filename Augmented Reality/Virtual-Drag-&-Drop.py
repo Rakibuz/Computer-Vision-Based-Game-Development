3ea47fd07af539fcd605891bd14cb0c478ef50b7
Md. Rakibuz Sultan Pranto <pranto055@gmail.com>
2022-06-06 14:11:43 +0600

    Update Virtual-Drag-&-Drop.py

--- Augmented Reality/Virtual-Drag-&-Drop.py
+++ Augmented Reality/Virtual-Drag-&-Drop.py
@@ -1,6 +1,8 @@
 import math
 import cv2
+import cvzone
 import mediapipe as mp
+import numpy as np
 
 cap=cv2.VideoCapture(0)
 cap.set(3,1280)
@@ -12,6 +14,23 @@ color=(255,0,255)
 
 cx,cy,w,h=100,100,200,200
 
+class DragRect():
+    def __init__(self, posCenter, size=[200, 200]):
+        self.posCenter = posCenter
+        self.size = size
+ 
+    def update(self, cursor):
+        cx, cy = self.posCenter
+        w, h = self.size
+ 
+        # If the index finger tip is in the rectangle region
+        if cx - w // 2 < cursor[0] < cx + w // 2 and cy - h // 2 < cursor[1] < cy + h // 2:
+            self.posCenter = cursor
+
+rectList = []
+for x in range(5):
+    rectList.append(DragRect([x * 250 + 150, 150]))
+
 
 def Hand_Finder(img,draw=True):
     if results.multi_hand_landmarks:
@@ -19,7 +38,6 @@ def Hand_Finder(img,draw=True):
             if draw:
                 mpDraw.draw_landmarks(img, handLms,mpHands.HAND_CONNECTIONS)
     return img
-
 
 def Finger_Detector(img,handNo=0,draw=True):
     landmark_List=[]
@@ -49,6 +67,7 @@ def tip_cliked(tip1,tip2):
             cv2.circle(img,((x1+x2)//2,(y1+y2)//2),10,(0,255,0),cv2.FILLED)
     return length
 
+
 while True:
 
     success,img=cap.read()
@@ -63,22 +82,25 @@ while True:
     if len(lmlist)!=0:
         length=tip_cliked(4,8)
         if length<48:
-            #cursor=[]
             cursor=lmlist[8][1:] #[8, 404, 406]  cursor is having [0,1,2] 1and 2 value
-            #cursor=lmlist[8]
-            #print(cursor[0])
-            if cx-w//2<cursor[0]<cx+w//2 and cy-h//2<cursor[1]<cy+h//2:
-                color=(0,255,0)
-                #print('Entered')
-                cx,cy=cursor
-            else:
-                color=(255,0,255)
+            for rect in rectList:
+                rect.update(cursor)
 
-    cv2.rectangle(img,(cx-w//2,cy-h//2),(cx+w//2,cy+h//2),color,cv2.FILLED)
-   
-
-
-    cv2.imshow("Image",img)
+      ## Draw Transperency
+    imgNew = np.zeros_like(img, np.uint8)
+    for rect in rectList:
+        cx, cy = rect.posCenter
+        w, h = rect.size
+        cv2.rectangle(imgNew, (cx - w // 2, cy - h // 2),
+                      (cx + w // 2, cy + h // 2), color, cv2.FILLED)
+        cvzone.cornerRect(imgNew, (cx - w // 2, cy - h // 2, w, h), 20, rt=0)
+ 
+    out = img.copy()
+    alpha = 0.1
+    mask = imgNew.astype(bool)
+    out[mask] = cv2.addWeighted(img, alpha, imgNew, 1 - alpha, 0)[mask]
+    
+    cv2.imshow("Image",out)
     if cv2.waitKey(5) & 0xFF == ord("q"):
                 break
   
