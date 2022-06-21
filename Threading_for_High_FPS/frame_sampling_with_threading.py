e1dc9a6a0fc38419ca202d775c44b4f2fdd551e6
Md. Rakibuz Sultan Pranto <pranto055@gmail.com>
2022-06-21 18:06:04 +0600

    Update frame_sampling_with_threading.py

--- Threading_for_High_FPS/frame_sampling_with_threading.py
+++ Threading_for_High_FPS/frame_sampling_with_threading.py
@@ -1,22 +1,15 @@
 from __future__ import print_function
 from imutils.video import WebcamVideoStream
-from imutils.video import FPS
-import imutils
 import cv2
 import time
 
- 
+#frame counter
 pTime=0
 cTime=0
 
 print("[INFO] sampling THREADED frames from webcam...")
+#webcamVideoStreamclass object creation
 vs = WebcamVideoStream(src=0).start()
-fps = FPS().start()
-
-
-print("[INFO] sampling frames from webcam...")
-stream = cv2.VideoCapture(0)
-fps = FPS().start()
  
 
 while True:
@@ -26,12 +19,14 @@ while True:
     cTime=time.time()
     fps=1/(cTime-pTime)
     pTime=cTime
-    print(fps)
+    #print(fps)
     cv2.putText(frame,str(int(fps)),(30,50),cv2.FONT_HERSHEY_PLAIN,3,(0,0,220),3) 
+
+    #display the frame
     cv2.imshow('Web Cam', frame)
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
 
 
-stream.release()
+vs.release()
 cv2.destroyAllWindows()
