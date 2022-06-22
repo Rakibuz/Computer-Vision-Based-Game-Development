7a19d0ac75211ce504d199b443803e1c8ea57268
Md. Rakibuz Sultan Pranto <pranto055@gmail.com>
2022-06-22 10:54:16 +0600

    zero division error

--- Threading_for_High_FPS/frame_sampling_with_threading.py
+++ Threading_for_High_FPS/frame_sampling_with_threading.py
@@ -3,6 +3,7 @@ from imutils.video import WebcamVideoStream
 import cv2
 import time
 
+
 #frame counter
 pTime=0
 cTime=0
@@ -15,18 +16,16 @@ vs = WebcamVideoStream(src=0).start()
 while True:
     frame = vs.read()
     #frame = imutils.resize(frame, width=400)
-
+    
     cTime=time.time()
     fps=1/(cTime-pTime)
     pTime=cTime
-    #print(fps)
+    print(int(fps))
     cv2.putText(frame,str(int(fps)),(30,50),cv2.FONT_HERSHEY_PLAIN,3,(0,0,220),3) 
 
     #display the frame
     cv2.imshow('Web Cam', frame)
-    if cv2.waitKey(1) & 0xff == ord('q'):
+    if cv2.waitKey(5) & 0xff == ord('q'):
         break
 
-
-vs.release()
-cv2.destroyAllWindows()
+ 
--- Threading_for_High_FPS/frame_sampling_without_threading.py
+++ Threading_for_High_FPS/frame_sampling_without_threading.py
@@ -21,7 +21,7 @@ while True:
     fps=1/(cTime-pTime)
     pTime=cTime
     cv2.putText(frame,str(int(fps)),(30,50),cv2.FONT_HERSHEY_PLAIN,3,(0,0,220),3) 
-    print(fps)
+    #print(fps)
     cv2.imshow('Web Cam', frame)
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
