232773e21048d7fef4039cd207a268980a79f695
Md. Rakibuz Sultan Pranto <pranto055@gmail.com>
2022-08-11 18:00:59 +0600

    Update eye_blinking_dlib.py

--- Eye Gaze Tracking/eye_blinking_dlib.py
+++ Eye Gaze Tracking/eye_blinking_dlib.py
@@ -10,6 +10,24 @@ predictor = dlib.shape_predictor("./Eye Gaze Tracking/shape_predictor_68_face_la
 def midpoint(p1 ,p2):
     return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
 
+font = cv2.FONT_HERSHEY_PLAIN
+
+def get_blinking_ratio(eye_points, facial_landmarks):
+
+    left_point = (facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y)
+    right_point = (facial_landmarks.part(eye_points[3]).x, facial_landmarks.part(eye_points[3]).y)
+    center_top = midpoint(facial_landmarks.part(eye_points[1]), facial_landmarks.part(eye_points[2]))
+    center_bottom = midpoint(facial_landmarks.part(eye_points[5]), facial_landmarks.part(eye_points[4]))
+
+    hor_line = cv2.line(frame, left_point, right_point, (255, 0, 0), 2)
+    ver_line = cv2.line(frame, center_top, center_bottom, (0, 255, 255), 2)
+
+    ver_line_lenght = hypot((center_top[0] - center_bottom[0]), (center_top[1] - center_bottom[1]))
+    hor_line_lenght = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
+
+    ratio = hor_line_lenght / ver_line_lenght
+
+    return ratio
 
 while True:
     ret,frame= cap.read()
@@ -29,13 +47,15 @@ while True:
         # y_land=landmarks.part(36).y
         # cv2.circle(frame,(x_land,y_land),3,(0,0,255),2)
 
-        left_point = (landmarks.part(36).x, landmarks.part(36).y)
-        right_point = (landmarks.part(39).x, landmarks.part(39).y)
-        center_top = midpoint(landmarks.part(37), landmarks.part(38))
-        center_bottom = midpoint(landmarks.part(41), landmarks.part(40))
+        left_eye_ratio = get_blinking_ratio([36, 37, 38, 39, 40, 41], landmarks)
+        right_eye_ratio = get_blinking_ratio([42, 43, 44, 45, 46, 47], landmarks)
+        blinking_ratio = (left_eye_ratio + right_eye_ratio) / 2
 
-        hor_line = cv2.line(frame, left_point, right_point, (255, 0, 0), 2)
-        ver_line = cv2.line(frame, center_top, center_bottom, (0, 255, 255), 2)
+        if blinking_ratio >5.7:
+            cv2.putText(frame,"BLINKING",(50,150),font,7,(255,255,0))
+
+
+        #print(ver_line_lenght)
 
    
     cv2.imshow("Frame",frame)
