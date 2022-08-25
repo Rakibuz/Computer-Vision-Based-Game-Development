959ba11a7930a968f242e5136386f308094dea06
Md. Rakibuz Sultan Pranto <pranto055@gmail.com>
2022-08-25 19:23:58 +0600

    Update attendance_project_mysql.py

--- Threading_for_High_FPS/attendance_project_mysql.py
+++ Threading_for_High_FPS/attendance_project_mysql.py
@@ -128,16 +128,28 @@ while True:
         
         matchIndex = np.argmin(faceDis)
 
-        if matches[matchIndex]:
+        # if matches[matchIndex]:
+        #     name = classNames[matchIndex].upper()
+        #     print(name)
+        #     y1,x2,y2,x1 = faceLoc
+        #     y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4 #for scaling
+        #     cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+        #     cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+        #     cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+        #     markAttendance(name)
+        #     markAttendance_sql(name)
+
+        if faceDis[matchIndex]< 0.50:
             name = classNames[matchIndex].upper()
-            print(name)
-            y1,x2,y2,x1 = faceLoc
-            y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4 #for scaling
-            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
-            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
-            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
             markAttendance(name)
             markAttendance_sql(name)
+        else: name = 'Unknown'
+        #print(name)
+        y1,x2,y2,x1 = faceLoc
+        y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+        cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+        cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+        cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
 
            
     cTime=time.time()
