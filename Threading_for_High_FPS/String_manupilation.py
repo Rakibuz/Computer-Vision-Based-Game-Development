7f1c1aab5c86cd88497c0586ac32cdd32c0ad70a
Md. Rakibuz Sultan Pranto <pranto055@gmail.com>
2022-09-01 12:10:28 +0600

    attendance_project_update

--- Threading_for_High_FPS/String_manupilation.py
+++ Threading_for_High_FPS/String_manupilation.py
@@ -1,6 +1,23 @@
+from datetime import datetime
+import csv
+
+
 url = "https://www.exaple.com/index.php?id=24124"
 id_number = url.split('=')[1]
 print(type(id_number))
 print(id_number)
 if(id_number=='24124'):
     print('hello')
+
+
+name=id_number
+now = datetime.now()
+Str_time = now.strftime("%H:%M:%S")
+Str_day = now.strftime("%m/%d/%Y")
+user=[name,Str_time,Str_day]
+
+#print(type(user))
+myFile = open('./Threading_for_High_FPS/Attendance.csv', 'r+')
+writer = csv.writer(myFile)
+writer.writerow(user)
+#myFile.close()
